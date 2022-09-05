9838963440d7419190dda8dbb1c0f1f832ebf8f2
Isiteketo Mutau <mutau85@gmail.com>
2022-09-05 16:10:09 +0200

    Add closebtn Javascript functionality

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const closeMobileItems = document.querySelectorAll('.closeMenu');
 const displayMobileMenu = () => {
   menuItem.style.display = 'block';
 };
+const closeMobileMenu = () => {
+  menuItem.style.display = 'none';
+};
+document.querySelector('.sidebar-toggle').addEventListener('click', displayMobileMenu);
+closeMobileItems.forEach(((element) => element.addEventListener('click', closeMobileMenu)));
+
